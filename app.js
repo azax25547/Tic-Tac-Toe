bc99fca679c4590dad268f5808fcba4592fe91ac
azax25547 <momentumangular@gmail.com>
2019-04-07 13:31:35 +0530

    Visualized

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+//Game Object
 var game = {
   isClicked: false,
-  crossGame: "X",
-  roundGame: "O",
+  crossGame: `<i class='fa fa-times'></i>`,
+  roundGame: `<i class='far fa-circle'></i>`,
   cross: [],
   round: [],
   winning_array: [
@@ -15,36 +16,26 @@ var game = {
     [3, 5, 7]
   ]
 };
+//output variable
+var output = document.getElementById("output");
 
+//get crosses and rounds from the user
 function getData(data, which_game, player) {
   var box = data.getAttribute("data-box");
   //console.log(box);
+  //switch between clicks
   if (!game.isClicked) {
     game.cross.push(box);
   } else if (game.isClicked) {
     game.round.push(box);
   }
+  //once clicked you cant click. So to disable the click
   data.removeAttribute("onclick");
   checkMoves(which_game, player);
 }
 
-function checkMoves(arr, player) {
-  var comp = arr.map(val => parseInt(val, 10)).sort();
-  // console.log(comp);
-  for (var i = 0; i < game.winning_array.length; i++) {
-    if (
-      comp.includes(game.winning_array[i][0]) &&
-      comp.includes(game.winning_array[i][1]) &&
-      comp.includes(game.winning_array[i][2])
-    ) {
-      console.log("Wins", player);
-    } else if (comp.length > 4) {
-      console.log("Draw");
-    }
-  }
-}
-
-function getSomething(data) {
+//simlutaneously display the click result and then operate it
+function takeTheInput(data) {
   if (!game.isClicked) {
     data.innerHTML = game.crossGame;
     getData(data, game.cross, game.crossGame);
@@ -55,3 +46,27 @@ function getSomething(data) {
     game.isClicked = !game.isClicked;
   }
 }
+
+//check every move and finnaly declare the winner
+function checkMoves(arr, player) {
+  var comp = arr.map(val => parseInt(val, 10)).sort();
+  console.log(comp);
+  for (var i = 0; i < game.winning_array.length; i++) {
+    if (
+      comp.includes(game.winning_array[i][0]) &&
+      comp.includes(game.winning_array[i][1]) &&
+      comp.includes(game.winning_array[i][2])
+    ) {
+      document.getElementById("outGrid").classList.add("disable");
+      output.innerHTML = `${player} wins the Game.<br/> Refresh to play Again <br/> <button onclick=window.location.reload();>Refresh</button>`;
+    }
+  }
+  if (game.cross.length + game.round.length == 9) {
+    output.innerHTML = `Game Draw`;
+  }
+}
+
+//after the game finishes the reload the browser
+const reload = () => {
+  window.location.reload;
+};
